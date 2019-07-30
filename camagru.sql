4b8f8a64517b49260495a57b9c901b81b86500b8
sophiebl <sophie@MacBook-Pro-de-Mac.local>
2019-07-30 10:31:55 +0200

    SQL

--- camagru.sql
+++ camagru.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8081
--- Generation Time: Jul 09, 2019 at 08:58 AM
--- Server version: 5.6.43
--- PHP Version: 5.6.40
+-- Hôte : localhost:3307
+-- Généré le :  mar. 30 juil. 2019 à 01:31
+-- Version du serveur :  5.7.25
+-- Version de PHP :  7.1.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `camagru`
+-- Base de données :  `camagru`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `articles`
+-- Structure de la table `articles`
 --
 
 CREATE TABLE `articles` (
@@ -36,7 +36,7 @@ CREATE TABLE `articles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `articles`
+-- Déchargement des données de la table `articles`
 --
 
 INSERT INTO `articles` (`id`, `title`, `content`, `date`) VALUES
@@ -46,87 +46,135 @@ INSERT INTO `articles` (`id`, `title`, `content`, `date`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `image`
+-- Structure de la table `image`
 --
 
 CREATE TABLE `image` (
-  `id_img` int(11) NOT NULL,
-  `img_name` varchar(800) NOT NULL
+  `id` int(11) NOT NULL,
+  `path` varchar(1000) NOT NULL,
+  `idUsers` int(11) NOT NULL,
+  `nbLike` int(11) NOT NULL,
+  `legend` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `image`
+--
+
+INSERT INTO `image` (`id`, `path`, `idUsers`, `nbLike`, `legend`) VALUES
+(1, '1564435300712.png', 13, 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `like`
+--
+
+CREATE TABLE `like` (
+  `id` int(11) NOT NULL,
+  `isLiked` int(11) NOT NULL,
+  `idUser` int(11) NOT NULL,
+  `IdImg` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
 CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
-  `isVerif` tinyint(1) NOT NULL DEFAULT '0'
+  `password` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
+  `isVerif` tinyint(1) NOT NULL DEFAULT '0',
+  `token` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `username`, `password`, `isVerif`) VALUES
-(1, 'test@testmail.com', 'Sophie', 'test', 0),
-(2, 'test2@testmail.com\r\n', 'Test', 'test', 0),
-(3, 'sophieboulaaouli@gmail.com', 'sophiebl', '123456789', 0),
-(5, 'sophieboulaaouli@gmail.com', 'sophiebl2', '123456789', 0),
-(7, 'sophieboulaaouli@gmail.com3', 'sophiebl3', '123456789', 0),
-(8, 'sophie.boulaaouli@gmail.com', 'sophiebl4', '123456789', 0),
-(9, 'soso@gmail.com', 'hello', '123456789', 0),
-(10, 'ri@gmail.com', 'toto', '123456789', 0),
-(11, 'testregister', 'test@register.com', '123456789', 0);
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `isVerif`, `token`) VALUES
+(1, 'test@testmail.com', 'Sophie', 'test', 0, 0),
+(2, 'test2@testmail.com\r\n', 'Test', 'test', 0, 0),
+(7, 'sophieboulaaouli@gmail.com3', 'sophiebl3', '123456789', 0, 0),
+(8, 'sophie.boulaaouli@gmail.com', 'sophiebl4', '123456789', 0, 0),
+(9, 'soso@gmail.com', 'hello', '123456789', 0, 0),
+(10, 'ri@gmail.com', 'toto', '123456789', 0, 0),
+(11, 'testregister', 'test@register.com', '123456789', 0, 0),
+(13, 'sophieboulaaouli@gmail.com', 'BL', 'd9e6762dd1c8eaf6d61b3c6192fc408d4d6d5f1176d0c29169bc24e71c3f274ad27fcd5811b313d681f7e55ec02d73d499c95455b6b5bb503acf574fba8ffe85', 1, 8969);
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `articles`
+-- Index pour la table `articles`
 --
 ALTER TABLE `articles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `image`
+-- Index pour la table `image`
 --
 ALTER TABLE `image`
-  ADD PRIMARY KEY (`id_img`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idUsers` (`idUsers`);
 
 --
--- Indexes for table `users`
+-- Index pour la table `like`
+--
+ALTER TABLE `like`
+  ADD KEY `idUser` (`idUser`),
+  ADD KEY `IdImg` (`IdImg`);
+
+--
+-- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `articles`
+-- AUTO_INCREMENT pour la table `articles`
 --
 ALTER TABLE `articles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `image`
+-- AUTO_INCREMENT pour la table `image`
 --
 ALTER TABLE `image`
-  MODIFY `id_img` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `image`
+--
+ALTER TABLE `image`
+  ADD CONSTRAINT `image_ibfk_1` FOREIGN KEY (`idUsers`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `like`
+--
+ALTER TABLE `like`
+  ADD CONSTRAINT `like_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `like_ibfk_2` FOREIGN KEY (`IdImg`) REFERENCES `image` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
