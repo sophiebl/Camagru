2f1e67953d3788acd047550cf281e844ddb23c20
sophiebl <sboulaao@student.42.fr>
2019-08-03 18:24:57 +0200

    JS LIKE/UNLIKE

--- public/js/post.js
+++ public/js/post.js
@@ -1,12 +1,17 @@
 var likes = document.getElementById('likes_nb');
 var heart = document.getElementById('heart');
 
-function likePost() {
-   heart.className =  'icon heart';
-   likes.innerText = parseInt(likes.innerText) + 1;
-   var xhttp = new XMLHttpRequest();
-   xhttp.open("GET", "?url=post&like=ok", true);
-   xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-   xhttp.send();
-   heart.setAttribute('onclick', 'unlikePost()');
+function likePost(imgId) {
+    heart.className =  'icon heart';
+    likes.innerText = parseInt(likes.innerText) + 1;
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "?url=post&imgId="+ imgId +"&like=ok", true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log('DONE');
+        }
+    };
+    xhttp.send();
+    //heart.setAttribute('onclick', 'unlikePost()');
 }
